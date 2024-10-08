65ffe729c985a24c62e2ebdd5f5fbdff98606022
12-ManiTej <121246541+12-ManiTej@users.noreply.github.com>
2024-10-08 21:48:19 +0530

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/1IZek4SA13hhIy0uvKkX7JJs8MS92usZJ/view?usp=sharing" download className="btn" style={{backgroundColor:'#0aff9d'}}>
+      <a href="https://drive.google.com/file/d/1A_JXZP2Jdb4I5zN8U8HgBX5kCSuO9JMN/view?usp=sharing" download className="btn" style={{backgroundColor:'#0aff9d'}}>
         Download CV
       </a>
       <a href="#contact" className="btn" style={{backgroundColor:'#0aff9d'}}>
